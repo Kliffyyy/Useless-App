d4a55c469c1f49b3d13ed86f1dccc76cd4e3078b
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-06-24 11:49:39 +0800

    Initial Commit

---
+++ Useless App/Useless_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useless_AppApp.swift
+//  Useless App
+//
+//  Created by klifton Cheng stu on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Useless_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
