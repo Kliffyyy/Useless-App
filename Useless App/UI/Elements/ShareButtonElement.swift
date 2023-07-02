1f44ee03acf5f70e4fc9e191425156fed1bc6828
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-07-02 09:04:12 +0800

    New folder with UI elements inside, new share button moved all views into ../UI/Views

---
+++ Useless App/UI/Elements/ShareButtonElement.swift
@@ -0,0 +1,25 @@
+//
+//  ShareButtonElement.swift
+//  Useless App
+//
+//  Created by klifton Cheng stu on 2/7/23.
+//
+
+import SwiftUI
+
+struct ShareButtonElement: View {
+    
+    let share: String
+    
+    var body: some View {
+        ShareLink(item: share) {
+            Image(systemName: "square.and.arrow.up")
+        }
+    }
+}
+
+struct ShareButtonElement_Previews: PreviewProvider {
+    static var previews: some View {
+        ShareButtonElement(share: "I want to share this fact")
+    }
+}
